2f8b647c63f0cddaf6b4c0ab5e8c7e29ee524a14
Onischuck Mikhail <onischuck.misha@yandex.ru>
2019-10-12 17:24:16 +0300

    Refactoring: divide get/put/delete handler into separate methods

--- src/main/java/ru/mail/polis/service/dog_m/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/dog_m/ServiceImpl.java
@@ -41,26 +41,19 @@ public class ServiceImpl extends HttpServer implements Service {
 
             final var key = ByteBuffer.wrap(id.getBytes(Charsets.UTF_8));
             switch (request.getMethod()) {
-                case Request.METHOD_GET: {
-                    try {
-                        final var value = dao.get(key).duplicate();
-                        return new Response(Response.OK, value.array());
-                    } catch (NoSuchElementException ex) {
-                        return new Response(Response.NOT_FOUND, Response.EMPTY);
-                    }
-                }
-                case Request.METHOD_PUT: {
-                    final var value = ByteBuffer.wrap(request.getBody());
-                    dao.upsert(key, value);
-                    return new Response(Response.CREATED, Response.EMPTY);
-                }
-                case Request.METHOD_DELETE: {
-                    dao.remove(key);
-                    return new Response(Response.ACCEPTED, Response.EMPTY);
-                }
+                case Request.METHOD_GET:
+                    return get(key);
+
+                case Request.METHOD_PUT:
+                    return put(key, request);
+
+                case Request.METHOD_DELETE:
+                    return delete(key);
+
                 default:
                     return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
             }
+            
         } catch (Exception ex) {
             return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
         }
@@ -74,5 +67,25 @@ public class ServiceImpl extends HttpServer implements Service {
     @Path("/v0/status")
     public Response status() {
         return new Response(Response.OK, Response.EMPTY);
+    }
+
+    private Response get(final ByteBuffer key) throws IOException {
+        try {
+            final var value = dao.get(key).duplicate();
+            return new Response(Response.OK, value.array());
+        } catch (NoSuchElementException ex) {
+            return new Response(Response.NOT_FOUND, Response.EMPTY);
+        }
+    }
+
+    private Response put(final ByteBuffer key, final Request request) throws IOException {
+        final var value = ByteBuffer.wrap(request.getBody());
+        dao.upsert(key, value);
+        return new Response(Response.CREATED, Response.EMPTY);
+    }
+
+    private Response delete(final ByteBuffer key) throws IOException {
+        dao.remove(key);
+        return new Response(Response.ACCEPTED, Response.EMPTY);
     }
 }

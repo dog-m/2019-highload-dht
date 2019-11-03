2484fb47275e9793215e5a047ac0372f405f190d
Onischuck Mikhail <onischuck.misha@yandex.ru>
2019-11-03 06:09:56 +0300

    Small tweaks for profiling script

--- profiling_results/do-profile.sh
+++ profiling_results/do-profile.sh
@@ -1,39 +1,73 @@
-# * wrk should be in /local/usr/bin (see wiki)
-# 1. './pre-profile.sh'
+# * wrk should be in /usr/local/bin (see [https://github.com/giltene/wrk2/wiki/Installing-wrk2-on-Linux])
+# 1. 'sudo ./pre-profile.sh'
 # 2. './gradlew run' in separate console
 # 3. './warm-up.sh'
-# 4. './do-profile.sh' in async-profiler root directory
+# 4. './do-profile.sh' in async-profiler root(!) directory
+
+### User-defined values ###
 
 STAGE=4
 TARGET='Cluster' # Cluster on 4, Server on 1..3
+OUTPUT_DIR='~'
+URL='http://localhost:8080'
+WRK_THREADS=2
+WRK_CONNECTIONS=10
+WRK_REQUESTS_PER_SECOND=1000
+WRK_SCRIPTS_DIR='~/2019-highload-dht/src/test/wrk'
+PROFILING_TYPES=(cpu alloc lock)
+HTTP_METHODS=(get put) # script names(!)
+# in seconds(!)
+PROFILE_TIME_WRK=60
+PROFILE_TIME_ASYNC_PROFILER=90
+RELAX_TIME=2.5 # hardware isn't just pile of cheap junk
 
-ID=`jps | grep $TARGET | cut -f 1 -d " "`
+### (not quite) user-defined values ###
 
 PREFIX="stage-$STAGE"
+WRK_PARAMS="-t${WRK_THREADS} -c${WRK_CONNECTIONS} -R${WRK_REQUESTS_PER_SECOND}"
+
+### *Magic* ###
 
 echo 'Reminder: are you already executed PRE-PROFILE and WARM-UP scripts?'
 echo "Stage prefix: $PREFIX"
 
-if [ "${ID}" == "" ]
+WRK_TIME=$(( ${PROFILE_TIME_WRK} + ${RELAX_TIME} ))
+ASYNC_PROFILER_TIME=$(( ${PROFILE_TIME_ASYNC_PROFILER} + ${RELAX_TIME} ))
+DURATION=$(( ${#HTTP_METHODS} * (${WRK_TIME} + ${ASYNC_PROFILER_TIME} * ${#PROFILING_TYPES}) ))
+DURATION=`date -d@${DURATION} -u +"%Mm %Ss"`
+
+PID=`jps | grep ${TARGET} | cut -f 1 -d " "`
+
+if [ "${PID}" == "" ]
 then
-  echo "Process $TARGET not found"
+  echo "Java process ${TARGET} not found"
+  exit
+
 else
-  echo "Found process $TARGET on id $ID"
+  echo "Found process ${TARGET} on id ${PID}"
+  echo "Have a cup of tee. It will take around ${DURATION}"
 
   echo 'Collecting WRK2 data...'
-    echo '[GET]'
-    wrk -t2 -c10 -d60s -R1000 -s ~/2019-highload-dht/src/test/wrk/get.lua --latency http://localhost:8080 > ~/${PREFIX}_wrk_get.log
-    echo '[PUT]'
-    wrk -t2 -c10 -d60s -R1000 -s ~/2019-highload-dht/src/test/wrk/put.lua --latency http://localhost:8080 > ~/${PREFIX}_wrk_put.log
+    for METHOD in "${HTTP_METHODS[@]}"
+    do
+      echo "[${METHOD}]"
+      wrk ${WRK_PARAMS} -d${PROFILE_TIME_WRK}s -s ${WRK_SCRIPTS_DIR}/${METHOD}.lua --latency ${URL} > ${OUTPUT_DIR}/${PREFIX}_wrk_${METHOD}.log
+      sleep ${RELAX_TIME}s
+    done
   echo 'WRK2 done'
 
   echo 'Collecting ASYNC-PROFILER data...'
-    echo '[GET]'
-    ./profiler.sh -d 90 -f ~/${PREFIX}_async-profiler_get.svg ${ID} &
-    wrk -t2 -c10 -d90s -R1000 -s ~/2019-highload-dht/src/test/wrk/get.lua http://localhost:8080 > /dev/null
-    echo '[PUT]'
-    ./profiler.sh -d 90 -f ~/${PREFIX}_async-profiler_put.svg ${ID} &
-    wrk -t2 -c10 -d90s -R1000 -s ~/2019-highload-dht/src/test/wrk/put.lua http://localhost:8080 > /dev/null
+    for METHOD in "${HTTP_METHODS[@]}"
+    do
+      for TYPE in "${PROFILING_TYPES[@]}"
+      do
+        echo "[${METHOD}] ${TYPE}"
+        ./profiler.sh -d ${PROFILE_TIME_ASYNC_PROFILER} -e ${TYPE} -f ${OUTPUT_DIR}/${PREFIX}_async-profiler_${METHOD}-${TYPE}.svg ${PID} &
+        wrk ${WRK_PARAMS} -d${PROFILE_TIME_ASYNC_PROFILER}s -s ${WRK_SCRIPTS_DIR}/${METHOD}.lua ${URL} > /dev/null
+        sleep ${RELAX_TIME}s
+      done
+    done
+  echo 'ASYNC-PROFILER done'
 
   echo 'All done'
 fi

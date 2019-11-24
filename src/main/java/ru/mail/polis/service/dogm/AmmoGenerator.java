510f43ff6e7f06c2b23349881768079deb5e5c4c
Onischuck Mikhail <onischuck.misha@yandex.ru>
2019-11-24 17:52:42 +0300

    Fix codeclimate. Attempt 1

--- src/main/java/ru/mail/polis/service/dogm/AmmoGenerator.java
+++ src/main/java/ru/mail/polis/service/dogm/AmmoGenerator.java
@@ -11,6 +11,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Logger;
 
@@ -19,12 +20,20 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 final class AmmoGenerator {
     private static final Logger log = Logger.getLogger(AmmoGenerator.class.getName());
     private static final int VALUE_LENGTH = 256;
+    private static final String CRLF = "\r\n";
+
+    private static final String STR_MAINCLASS = "ru.mail.polis.service.dogm.AmmoGenerator";
+    private static final String STR_CLASSPATH = "-cp build/classes/java/main " + STR_MAINCLASS;
+    private static final String STR_METHODS = "put|put-o|get|get-r|mix";
+    private static final String STR_PARAMS = "<" + STR_METHODS + "> <requests>";
+    private static final String STR_USAGE = "Usage:\n\tjava " + STR_CLASSPATH + " " + STR_PARAMS;
+
     private final String mode;
     private final int count;
     private final PrintStream out;
 
     private AmmoGenerator(final String mode, final int count) throws FileNotFoundException {
-        if (!"put|put-o|get|get-r|mix".contains(mode)) {
+        if (!STR_METHODS.contains(mode)) {
             throw new UnsupportedOperationException("Unsupported mode: " + mode);
         }
 
@@ -49,26 +58,26 @@ final class AmmoGenerator {
         final ByteArrayOutputStream request = new ByteArrayOutputStream();
         try (Writer writer = new OutputStreamWriter(request, US_ASCII)) {
             writer.write("PUT /v0/entity?id=" + key + " HTTP/1.1\r\n");
-            writer.write("Content-Length: " + value.length + "\r\n");
-            writer.write("\r\n");
+            writer.write("Content-Length: " + value.length + CRLF);
+            writer.write(CRLF);
         }
         request.write(value);
         out.write(Integer.toString(request.size()).getBytes(US_ASCII));
         out.write(" put\n".getBytes(US_ASCII));
         request.writeTo(out);
-        out.write("\r\n".getBytes(US_ASCII));
+        out.write(CRLF.getBytes(US_ASCII));
     }
 
     private void get(final String key) throws IOException {
         final ByteArrayOutputStream request = new ByteArrayOutputStream();
         try (Writer writer = new OutputStreamWriter(request, US_ASCII)) {
             writer.write("GET /v0/entity?id=" + key + " HTTP/1.1\r\n");
-            writer.write("\r\n");
+            writer.write(CRLF);
         }
         out.write(Integer.toString(request.size()).getBytes(US_ASCII));
         out.write(" get\n".getBytes(US_ASCII));
         request.writeTo(out);
-        out.write("\r\n".getBytes(US_ASCII));
+        out.write(CRLF.getBytes(US_ASCII));
     }
 
     private void putUnique() throws IOException {
@@ -83,7 +92,7 @@ final class AmmoGenerator {
         }
     }
 
-    private ArrayList<String> randomKeys(final int amount) {
+    private List<String> randomKeys(final int amount) {
         final var keys = new ArrayList<String>(amount);
         for (int i = 0; i < amount; i++) {
             keys.add(randomKey());
@@ -100,7 +109,7 @@ final class AmmoGenerator {
                 put(key, randomValue());
                 existingKeys.add(key);
             } else {
-                int index = ThreadLocalRandom.current().nextInt(0, existingKeys.size());
+                final int index = ThreadLocalRandom.current().nextInt(0, existingKeys.size());
                 get(existingKeys.get(index));
             }
         }
@@ -109,7 +118,7 @@ final class AmmoGenerator {
     private void putWithOverwrite() throws IOException {
         final var keys = randomKeys(count);
         final int repeatCount = count / 10;
-        for(int i = 0; i < repeatCount; i++) {
+        for (int i = 0; i < repeatCount; i++) {
             final var index = ThreadLocalRandom.current().nextInt(count);
             final var key = keys.get(ThreadLocalRandom.current().nextInt(count));
             keys.set(index, key);
@@ -159,9 +168,9 @@ final class AmmoGenerator {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException {
         if (args.length != 2) {
-            log.severe("Usage:\n\tjava -cp build/classes/java/main ru.mail.polis.service.<login>.AmmoGenerator <put|get> <requests>");
+            log.info(STR_USAGE);
             System.exit(-1);
         }
 
